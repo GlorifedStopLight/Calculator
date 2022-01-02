8ef959136575f6617264a1f796d377a5e98a0984
Mark Hanna <glorifedstoplight@gmail.com>
2022-01-01 19:19:43 -0500

    cleaned up base script. Now there is a function to call on that will do all the print statements and humanReadable and fullSplice all that nonsense. A variable currently called options was made in the base script to easily and quickly show off some of my calculator's features

--- Base.py
+++ Base.py
@@ -1,18 +1,30 @@
-from MarMath import polyLongDivision, humanReadableEquation
 from MarSplicer import spliceFullEquation
+from MarMath import *
+from math import inf
 
-rawEquation1 = "3x^{4} - 4x^{2} + 1"
 
-rawEquation2 = "x + 1"
+# answers the problems that the user gives it
+def slaveToUser(funMath, listOfInputs):
 
-print(humanReadableEquation(spliceFullEquation(rawEquation1)))
-print("/")
-print(humanReadableEquation(spliceFullEquation(rawEquation2)))
+    # splice the user's input
+    listOfSplicedInput = [spliceFullEquation(listOfInputs[i]) for i in range(len(listOfInputs))]
 
-newPoly = polyLongDivision(spliceFullEquation(rawEquation1), spliceFullEquation(rawEquation2))
+    # answer question
+    answerToQuestion = funMath(*listOfSplicedInput)
 
-print()
-print(humanReadableEquation(newPoly))
+    # change it so that the answer is readable
+    answerToQuestion = humanReadableEquation(answerToQuestion)
 
-#print(findAddToMultiplyTo(1, 5*-18))
-#print(findGreatestCommonMultiple([35, 49]))
+    # show answer
+    print(answerToQuestion)
+
+
+# [0] = function name   [1] = test input   [2] = number of arguments (inf = any number)
+options = [
+    [polyLongDivision, ["3x^{4} - 4x^{2} + 1", "x + 1"], 2],
+    [findAddToMultiplyTo, [[-3, 9]], 1],
+    [findGreatestCommonMultiple, [9, 27, 12], inf]
+]
+
+
+slaveToUser(options[0][0], options[0][1])
